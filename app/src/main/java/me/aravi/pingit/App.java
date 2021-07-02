e50c00c14582b82d0a3187a84f698d1f94c2dbd0
Aravind Chowdary <kamaravichow@gmail.com>
2021-07-02 12:44:46 +0530

    [new] application class

---
+++ app/src/main/java/me/aravi/pingit/App.java
@@ -0,0 +1,16 @@
+package me.aravi.pingit;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
